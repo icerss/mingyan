9452464f52aeb869b2e96569f23fb1e189844b81
xhemj <xhemj2680@163.com>
2020-10-11 17:09:05 +0800

    add push

--- src/main.js
+++ src/main.js
@@ -8,6 +8,18 @@ var _hmt = _hmt || []; (function () {
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
 })();
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https'){
+   bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+  }
+  else{
+  bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+  }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
 var footer = $("footer").html().replace("$1", mingyan.length);
 var ua = new Browser();
 $("#showall").hide();
