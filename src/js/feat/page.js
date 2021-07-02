275184da226a6c81795713e899ae5d87216637f6
xhemj <xhemj2680@163.com>
2021-07-02 11:58:24 +0800

    紧急修复md问题

--- src/js/feat/page.js
+++ src/js/feat/page.js
@@ -3,25 +3,27 @@ import { $page, $main, $search, $footer } from "../init";
 import { MY_initLogo as _initLogo } from "./initLogo";
 import { ___date_version___ } from "./config";
 
-import MORE_MD from "../../page/more.md";
-
 /**
  * Markdown转成网页
  * @param {String} id 要放的元素id
  * @param {String} url Markdown地址
  */
-let _mdToHtml = function (id, name) {
+let _mdToHtml = function (id, url) {
     $($page).hide();
     $($main).hide();
     $($search).hide();
     $(id).html("<strong>" + "<div style=\"text-align: center\" class=\"mdui-ripple\"><style>h1{font-size:30px}</style><h1>加载中……</h1></div></strong></br></br></br></br>");
-    marked.setOptions({
-        breaks: true
-    });
-    let html = marked(name); // 使用marked.js转换
-    html = html.replace(/<a /g, "<a target=\"_blank\" rel=\"nofollow ugc\" "); // 外链新页面打开
-    $(id).html("<strong>" + html + "</strong></br></br></br></br>");  // 加换行比较好看
-    $(id).fadeIn();
+    fetch(url)
+        .then(res => res.text())
+        .then(function (data) {
+            marked.setOptions({
+                breaks: true
+            });
+            let html = marked(data); // 使用marked.js转换
+            html = html.replace(/<a /g, "<a target=\"_blank\" rel=\"nofollow ugc\" "); // 外链新页面打开
+            $(id).html("<strong>" + html + "</strong></br></br></br></br>");  // 加换行比较好看
+            $(id).fadeIn();
+        });
     $($footer).html("当前名言数量：" + mingyan.length + "</br><a class=\"aline\" id=\"clearhash_btn\" href=javascript:;>返回<\/a>"); // 更改footer
     _initLogo();
     $($page).fadeIn();
@@ -32,7 +34,7 @@ let _mdToHtml = function (id, name) {
  */
 export let MY_page = {
     "more": function () {
-        return _mdToHtml($page, MORE_MD);
+        return _mdToHtml($page, "./src/page/more.md?t=" + ___date_version___);
     },
     "about": function () {
         return _mdToHtml($page, "./src/page/about.md?t=" + ___date_version___);
